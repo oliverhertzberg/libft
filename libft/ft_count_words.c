bceecdc44eddbd83783953e90da5214600fafb4a
Oliver Hertzberg <98164743+oliverhertzberg@users.noreply.github.com>
2024-01-03 20:25:55 +0200

    Add files via upload

---
+++ libft/ft_count_words.c
@@ -0,0 +1,15 @@
+int	ft_count_words(char const *s, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] != c && (i == 0 || (s[i - 1] == c)))
+			count++;
+		i++;
+	}
+	return (count);
+}
